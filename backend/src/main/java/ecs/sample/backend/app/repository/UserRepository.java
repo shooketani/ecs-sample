af587847ef11b6e1855462de5d8d43824517baa7
shooketani <xxxxxx@gmail.com>
2023-02-22 00:42:32 +0900

    entityscan

--- backend/src/main/java/ecs/sample/backend/app/repository/UserRepository.java
+++ backend/src/main/java/ecs/sample/backend/app/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package ecs.sample.backend.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import ecs.sample.backend.app.model.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
--- backend/src/main/java/ecs/sample/backend/config/App.java
+++ backend/src/main/java/ecs/sample/backend/config/App.java
@@ -3,11 +3,13 @@ package ecs.sample.backend.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 // import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EntityScan("ecs.sample.backend.app.model.entity")
+@EnableJpaRepositories(basePackages="ecs.sample.backend.app.repository")
 public class App {
 	
     public static void main(String[] args) {
