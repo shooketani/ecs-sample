620d8855b9a4b8708f7e3577e00b529131267ce2
shooketani <xxxxxx@gmail.com>
2023-02-22 00:36:03 +0900

    entityscan

--- backend/src/main/java/ecs/sample/backend/config/App.java
+++ backend/src/main/java/ecs/sample/backend/config/App.java
@@ -3,9 +3,11 @@ package ecs.sample.backend.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 // import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EntityScan("ecs.sample.backend.app.model.entity")
 public class App {
 	
     public static void main(String[] args) {
